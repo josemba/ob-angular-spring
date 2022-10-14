c561d24dce23c9e7c17c3e0806f95bd21b744a48
josemba <josemba@hotmail.com>
2022-10-14 20:36:53 +0200

    Cambios en la constante API_URL para ver si es por eso por lo que no redirige.

--- src/app/_services/hello.service.ts
+++ src/app/_services/hello.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'https://ob-spring-security-jwt-josemba.herokuapp.com/api/';
+const API_URL = 'https://ob-spring-security-jwt-josemba.herokuapp.com/';
 //const API_URL = 'http://localhost:8080/api/';
 
 @Injectable({
